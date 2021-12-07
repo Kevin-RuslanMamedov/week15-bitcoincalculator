1b66191ee17388189f7acef60f378c2595333911
Opilane TTHK <opilane@tthk.ee>
2021-12-07 15:58:47 +0200

    week15-bitcoincalculator

---
+++ BitCoinCalculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace BitCoinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitCoinRate currentBitcoin = GetRates();
+            Console.WriteLine($"Current rate: {currentBitcoin.bpi.EUR.code} {currentBitcoin.bpi.EUR.rate_float}");
+
+            Console.WriteLine("Cakculate in: EUR/USD/GBP");
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("Enter the amount of bitcoin");
+            float userCoins = float.Parse(Console.ReadLine());
+            float currentRate = 0;
+
+            if(userChoice == "EUR")
+            {
+                currentRate = currentBitcoin.bpi.EUR.rate_float;
+            }
+            float result = currentRate * userCoins;
+            Console.WriteLine($"Your bitcoin are worth {result}{userChoice}");
+
+
+            //program küsib, mitu bitcoini kasutajal on
+            //program küsib, mis valuutas (EUR/USD/GBP) ta arvutab tulemus
+            //program kuvab tulemuse konsoolis
+        }
+        public static BitCoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitCoinRate bitcoinData;
+
+            using(var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoinData = JsonConvert.DeserializeObject<BitCoinRate>(response);
+            }
+            return bitcoinData;
+        }
+    }
+}
